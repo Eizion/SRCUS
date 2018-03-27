6ed34392bcdf454d28709b0d958a0a258e2af43e
gm37732 <gm37732@RECIRSC.msmyid.uga.edu>
2018-03-27 13:25:46 -0400

    commit

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -1,52 +1,64 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dbHelpers.UpdateCredsQuery;
+import model.User;
+import utilities.Encryption;
 
 /**
- * Servlet implementation class UpdateServlet
+ * Servlet implementation class UpdatePasswordServlet
  */
-@WebServlet("/update")
+@WebServlet({ "/UpdateServlet", "/updateProfile" })
 public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private String url;
        
-   
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("email");
-		String fName = request.getParameter("fname");
-		String lName = request.getParameter("lname");
-		
-		try {
-		    //Setup the Database datasource
-		    Context    ctx = new InitialContext();
-		    Context env = ( Context )ctx.lookup( "java:comp/env" );
-		    DataSource ds = ( DataSource )env.lookup( "jdbc/srcus_master");
-		    Connection conn = ds.getConnection();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-		    //Prepare the SQL statmenet to insert the values
-		    PreparedStatement stmt = conn.prepareStatement("UPDATE user(uname, fname, lname)  VALUES (?,?,?)");
-		    stmt.setString(1, email);
-		    stmt.setString(2, fname);
-		    stmt.setString(3, lname);
-	
-
-		    //Execute the insert
-		    stmt.executeUpdate();
-		    conn.close();
-
-		    //Dispatch into success page
-		    RequestDispatcher requestDispatcher = req.getRequestDispatcher("viewProfile.jsp");
-		    requestDispatcher.forward(req, res);
-		}
-		    catch(Exception e){
-		        System.out.println(e);
-		    }
-	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
 	}
-	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		
+		//Getting variables and whatnot
+		User user = (User) session.getAttribute("user");
+		String email = user.getEmail();
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		String errorMessage = "";
+		
+
+		
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);	
+		
+	}
 
 }
